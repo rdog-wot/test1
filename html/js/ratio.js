2785c4418d0ef49ec0035b2dca7d9ba782b4a568
rdog-wot <rdog.wot@gmail.com>
2020-01-22 11:08:50 +0900

    html

---
+++ html/js/ratio.js
@@ -0,0 +1,16 @@
+
+$(window).load(function(){
+
+    $('#ick50').click(function() {
+        $("body").css("zoom","75%");
+    });
+    $('#ick100').click(function() {
+        $("body").css("zoom","100%");
+    });
+    $('#ick150').click(function() {
+        $("body").css("zoom","125%");
+    });
+    $('#ick200').click(function() {
+        $("body").css("zoom","150%");
+    });
+});
---
+++ html/js/ratio2.js
@@ -0,0 +1,5 @@
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+  ga('create', 'UA-134546377-1', 'auto');
+        // Plugins
+  ga('send', 'pageview');
+        
